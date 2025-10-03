a56ac37c5f0c6b77aff4c8a4533378f5e456c699
MoHa270xX <mhmdmedo102030@gmail.com>
2025-10-03 17:49:15 +0300

    create welcome screen

---
+++ lib/features/weather/presentation/widgets/containu_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ContinueButton extends StatelessWidget {
+  const ContinueButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          // Navigate to the LocationTimeScreen when pressed
+         
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF1E3A8A),
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        child: const Text(
+          'Continue as Guest',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
