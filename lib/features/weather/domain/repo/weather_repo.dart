dedfa1f41f13528a3e94beaa636b1ffd6d0a0d11
MoHa270xX <mhmdmedo102030@gmail.com>
2025-10-04 00:03:47 +0300

    create use case and handel the repo to take parameters

--- lib/features/weather/domain/repo/weather_repo.dart
+++ lib/features/weather/domain/repo/weather_repo.dart
@@ -3,5 +3,10 @@ import 'package:weather_project/core/errors/failure.dart';
 import 'package:weather_project/features/weather/domain/entities/weather_entity.dart';
 
 abstract class WeatherRepo {
-  Future<Either<Failure,List<WeatherEntity>>> getWeatherData();
+  Future<Either<Failure, WeatherEntity>> getWeatherData({
+    required double latitude,
+    required double longitude,
+    required DateTime date,
+    required bool higherAccuracy,
+  });
 }
---
+++ lib/features/weather/domain/usecases/weather_use_case.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:weather_project/core/errors/failure.dart';
+import 'package:weather_project/core/use_cases/use_case.dart';
+import 'package:weather_project/features/weather/domain/entities/weather_entity.dart';
+import 'package:weather_project/features/weather/domain/repo/weather_repo.dart';
+
+class WeatherUseCase extends UseCase<WeatherEntity,WeatherParams> {
+  final WeatherRepo weatherRepo;
+
+  WeatherUseCase({required this.weatherRepo});
+  @override
+
+    Future<Either<Failure, WeatherEntity>> call(WeatherParams params) {
+      return weatherRepo.getWeatherData(
+        latitude: params.latitude,
+        longitude: params.longitude,
+        date: params.date,
+       higherAccuracy: false,
+      );
+    }
+  }
+
+
+class WeatherParams {
+  final double latitude;
+  final double longitude;
+  final DateTime date;
+  final bool higherAccuracy;
+
+  WeatherParams(this.higherAccuracy, {
+    required this.latitude,
+    required this.longitude,
+    required this.date,
+  });
+}
