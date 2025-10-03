88689b2f7a1133c86929e84d6b84d78539cdbeda
MoHa270xX <mhmdmedo102030@gmail.com>
2025-10-03 18:29:21 +0300

    create main screen and navigation bar

---
+++ lib/features/weather/presentation/pages/main_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:weather_project/features/weather/presentation/pages/Map_screen.dart';
+import 'package:weather_project/features/weather/presentation/pages/dashboard_screen.dart';
+import 'package:weather_project/features/weather/presentation/pages/export_data.dart';
+import 'package:weather_project/features/weather/presentation/pages/settings_screen.dart';
+import 'package:weather_project/features/weather/presentation/widgets/custom_nav_bar.dart';
+import 'package:weather_project/features/weather/presentation/widgets/location_screen_body.dart';
+
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedNavIndex = 0;
+   
+  final List<Widget> _screens = const [
+   
+    LocationTimeScreenBody(),
+    MapScreen(mode: MapMode.dropPin),
+    WeatherProbabilitiesScreen(),
+    ExportDataScreen(),
+    SettingScreen(),
+  ];
+
+  void _onNavItemTapped(int index) {
+    setState(() {
+      _selectedNavIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedNavIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: CustomBottomNavBar(currentIndex:_selectedNavIndex , onTap: _onNavItemTapped)
+    );
+  }
+}
